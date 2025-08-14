d46d3f31b8df8aff39f744d454356eea8e09830d
mohammadsofan <nassersofan@gmail.com>
2025-08-14 13:16:16 +0300

    feat: add orderitems controller with validation using data annotations, add authorization, edit orders controller authorization

--- OrderProcessingSystem.Api/Dtos/Requests/OrderItemRequestDto.cs
+++ OrderProcessingSystem.Api/Dtos/Requests/OrderItemRequestDto.cs
@@ -1,13 +1,20 @@
 ﻿using OrderProcessingSystem.Api.Dtos.Responses;
 using OrderProcessingSystem.Api.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace OrderProcessingSystem.Api.Dtos.Requests
 {
     public class OrderItemRequestDto
     {
+        [Required(ErrorMessage = "Order ID is required.")]
         public int OrderId { get; set; }
+        [Required(ErrorMessage = "Product ID is required.")]
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "Quantity is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be 1 or more.")]
         public int Quantity { get; set; }
+        [Required(ErrorMessage = "Total price is required.")]
+        [Range(0.00, double.MaxValue, ErrorMessage = "Total price must be a positive value.")]
         public decimal TotalPrice { get; set; }
     }
 }
