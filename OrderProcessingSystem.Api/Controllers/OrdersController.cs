200e8c2d498979f46f1c3a5d6d0cef5eedf24841
mohammadsofan <nassersofan@gmail.com>
2025-08-14 13:06:12 +0300

    feat: add orders controller with validation using data annotations, add authorization

---
+++ OrderProcessingSystem.Api/Controllers/OrdersController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderProcessingSystem.Api.Dtos.Requests;
+using OrderProcessingSystem.Api.Enums;
+using OrderProcessingSystem.Api.Interfaces.IService;
+using OrderProcessingSystem.Api.Wrappers;
+
+namespace OrderProcessingSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrdersController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrdersAsync()
+        {
+            var result = await _orderService.GetAllByFilterAsync();
+            if (result.IsSuccess)
+            {
+                return Ok(ApiResponse.Success(result.Message, result.Data));
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Failure("Unexpected error occured"));
+        }
+
+        [HttpGet("{id}", Name = "GetOrderByIdAsync")]
+        public async Task<IActionResult> GetOrderByIdAsync(int id)
+        {
+            var result = await _orderService.GetOneByFilterAsync(o => o.Id == id);
+            if (result.IsSuccess)
+            {
+                return Ok(ApiResponse.Success(result.Message, result.Data));
+            }
+            else if (result.Status == ServiceResultStatus.NotFound)
+            {
+                return NotFound(ApiResponse.Failure("Order not found"));
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Failure("Unexpected error occured"));
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> AddOrderAsync([FromBody] OrderRequestDto orderRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ApiResponse.Failure("Invalid request", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList()));
+            }
+            var result = await _orderService.AddAsync(orderRequest);
+            if (result.IsSuccess)
+            {
+                return CreatedAtRoute(nameof(GetOrderByIdAsync), new { id = result.Data?.Id }, result.Data);
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Failure("Unexpected error occured"));
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOrderAsync(int id, [FromBody] OrderRequestDto orderRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ApiResponse.Failure("Invalid request", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList()));
+            }
+            var result = await _orderService.UpdateAsync(id, orderRequest);
+            if (result.IsSuccess)
+            {
+                return NoContent();
+            }
+            else if (result.Status == ServiceResultStatus.NotFound)
+            {
+                return NotFound(ApiResponse.Failure("Order not found"));
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Failure($"Unexpected error occured {result.Message}"));
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderAsync(int id)
+        {
+            var result = await _orderService.DeleteAsync(id);
+            if (result.IsSuccess)
+            {
+                return NoContent();
+            }
+            else if (result.Status == ServiceResultStatus.NotFound)
+            {
+                return NotFound(ApiResponse.Failure("Order not found"));
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Failure("Unexpected error occured"));
+        }
+    }
+}
--- OrderProcessingSystem.Api/Dtos/Requests/OrderRequestDto.cs
+++ OrderProcessingSystem.Api/Dtos/Requests/OrderRequestDto.cs
@@ -1,14 +1,21 @@
 ﻿using OrderProcessingSystem.Api.Enums;
 using OrderProcessingSystem.Api.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace OrderProcessingSystem.Api.Dtos.Requests
 {
     public class OrderRequestDto
     {
+        [Required(ErrorMessage = "User ID is required.")]
         public int UserId { get; set; }
         public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        [Required(ErrorMessage = "Total amount is required.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Total amount must be a positive value.")]
         public decimal TotalAmount { get; set; }
+        [Required(ErrorMessage = "Phone number is required.")]
+        [Phone(ErrorMessage = "Invalid phone number format.")]
         public string PhoneNumber { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Shipping address is required.")]
         public string ShippingAddress { get; set; } = string.Empty;
         public OrderStatus Status { get; set; } = OrderStatus.Pending;
 
