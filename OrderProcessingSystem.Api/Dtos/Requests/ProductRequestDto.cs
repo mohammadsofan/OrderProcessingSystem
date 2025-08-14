0d9badc4eb3f7c1fa61ff1ffb8305b931f5673d3
mohammadsofan <nassersofan@gmail.com>
2025-08-14 12:59:30 +0300

    feat: add products controller with validation using data annotations, add authorization

--- OrderProcessingSystem.Api/Dtos/Requests/ProductRequestDto.cs
+++ OrderProcessingSystem.Api/Dtos/Requests/ProductRequestDto.cs
@@ -1,10 +1,20 @@
-﻿namespace OrderProcessingSystem.Api.Dtos.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderProcessingSystem.Api.Dtos.Requests
 {
     public class ProductRequestDto
     {
+        [Required(ErrorMessage = "Product name is required.")]
+        [MinLength(3, ErrorMessage = "Product name must be at least 3 characters long.")]
         public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Product description is required.")]
+        [MinLength(3, ErrorMessage = "Product description must be at least 3 characters long.")]
         public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.00, double.MaxValue, ErrorMessage = "Price must be a positive value.")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Stock quantity is required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock quantity must be a non-negative integer.")]
         public int StockQuantity { get; set; }
     }
 }
